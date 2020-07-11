882ecceeac3cfdfa7255b94a59c9ed9cebd18e30
SAGUN JAISWAL <63302439+sagunjaiswal@users.noreply.github.com>
2020-07-11 10:45:27 +0530

    Update buttonIncrement.js

--- DAY-03/buttonIncrement.js
+++ DAY-03/buttonIncrement.js
@@ -1,10 +1,3 @@
-// function increment(){
-//     var i=0;
-//     let num = document.getElementById('.btn .num').value=i++;
-//     console.log(num);
-//     // num++;
-
-// }
 function incrementValue()
 {
     var value = document.getElementById('number').value;
