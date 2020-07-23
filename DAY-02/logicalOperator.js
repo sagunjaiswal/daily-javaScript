7adac10557b2b82927ab1c7cbd9f9e60e18719d5
SAGUN JAISWAL <sagunjaiswal99@gmail.com>
2020-07-23 11:04:57 +0530

    Add files via upload

---
+++ DAY-02/logicalOperator.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 20;
+//AND OPERATOR
+document.write(a < 9 && b > 1, "<br/>"); //returns false
+//OR OPERATOR
+document.write(a === 10 || b === 20, "<br/>"); //returns true
+//NOT OPERATOR
+document.write(!(a === b)); //returns true
